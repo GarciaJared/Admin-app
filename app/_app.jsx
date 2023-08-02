8b7c99955378a3e5779ddbaed950943cc28c6cd1
yeyeJared <gjared388@gmail.com>
2023-08-02 14:25:59 -0600

    [ADD] initial UI components

---
+++ app/_app.jsx
@@ -0,0 +1,14 @@
+import MainLayout from '@layout/layout';
+import '@styles/tailwind.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  );
+}
+
+export default MyApp;
